44c144dd85b45cd9937a1a659c7125175e39fe37
Baran Güngör <baran.gungor@modanisa.com>
2020-09-16 16:35:00 +0300

    Add TopView in SearchView

---
+++ KickSearch/KickSearch/Scenes/SearchView/SearchViewPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewPresenter.swift
+//  KickSearch
+//
+//  Created by Baran on 16.09.2020.
+//  Copyright © 2020 Baran. All rights reserved.
+//
+
+import Foundation
+
+class SearchViewPresenter {
+    
+    
+    
+}

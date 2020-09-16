dcace2cc4dbefff57358ba3626efd1b59eaa2b71
Baran Güngör <baran.gungor@modanisa.com>
2020-09-16 17:32:08 +0300

    Add URLCache in Requests folder

--- KickSearch/KickSearch/API/Requests.swift
+++ KickSearch/KickSearch/API/Requests.swift
@@ -11,28 +11,43 @@ import Foundation
 class Request {
     
     static let shared = Request()
-
+    
     func getData(_ completionHandler: @escaping(_ isSuccess: Bool, _ data: [MainResponse]?) -> Void){
-          let session = URLSession.shared
-          let url = URL(string: "http://starlord.hackerearth.com/kickstarter")!
-          let task = session.dataTask(with: url, completionHandler: { data, response, error in
-              
-              if error != nil {
-                  completionHandler(false, nil)
-                  return
-              }
-              
-              do {
-                  let json = try JSONDecoder().decode([MainResponse].self, from: data! )
-                  completionHandler(true, json)
-              } catch {
-                  print("Error during JSON serialization: \(error.localizedDescription)")
-                  completionHandler(false, nil)
-              }
-              
-          })
-          task.resume()
-          
-      }
-
+        
+        guard let url = URL(string: "http://starlord.hackerearth.com/kickstarter") else { return }
+        
+        let session = URLSession.shared
+        let cache = URLCache.shared
+        let request = URLRequest(url: url)
+        
+        if let data = cache.cachedResponse(for: request)?.data {
+            
+            let json = try? JSONDecoder().decode([MainResponse].self, from: data)
+            completionHandler(true, json)
+            
+        } else {
+            session.dataTask(with: url, completionHandler: { data, response, error in
+    
+                if error != nil {
+                    completionHandler(false, nil)
+                    return
+                }
+                
+                // Look Here After!
+                do {
+                    if let response = response, let data = data {
+                        let cachedData = CachedURLResponse(response: response, data: data)
+                        cache.storeCachedResponse(cachedData, for: request)
+                        let json = try JSONDecoder().decode([MainResponse].self, from: data)
+                        completionHandler(true, json)
+                    }
+                } catch {
+                    print("Error during JSON serialization: \(error.localizedDescription)")
+                    completionHandler(false, nil)
+                }
+            })
+                .resume()
+        }
+    }
+    
 }
